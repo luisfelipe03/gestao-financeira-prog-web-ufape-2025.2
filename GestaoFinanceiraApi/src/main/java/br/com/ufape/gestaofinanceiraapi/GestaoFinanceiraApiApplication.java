6788f927367b2d2f34ed2f04352eb7b8027163f9
luisfelipe03 <llipeandrade34@gmail.com>
2025-12-02 12:37:40 -0300

    init: Projeto inicial

---
+++ GestaoFinanceiraApi/src/main/java/br/com/ufape/gestaofinanceiraapi/GestaoFinanceiraApiApplication.java
@@ -0,0 +1,13 @@
+package br.com.ufape.gestaofinanceiraapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GestaoFinanceiraApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GestaoFinanceiraApiApplication.class, args);
+    }
+
+}
---
+++ GestaoFinanceiraApi/src/test/java/br/com/ufape/gestaofinanceiraapi/GestaoFinanceiraApiApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.ufape.gestaofinanceiraapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GestaoFinanceiraApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
